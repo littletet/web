986e0c27ba131b591761a190ef9d25590b5ec471
littletet <2431693839@qq.com>
2023-03-30 14:58:26 +0800

    第一次提交

---
+++ src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>欢迎 {{ user }} 进入电商后台管理系统</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: ''
+    }
+  },
+  created() {
+    this.user = sessionStorage.getItem('user')
+  }
+}
+</script>
+
+<style scoped></style>
